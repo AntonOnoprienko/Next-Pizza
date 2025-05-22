6e7e1b2eaabc07ed8fb232bc1f59519dbf67f3e5
Anton <96301510+AntonOnoprienko@users.noreply.github.com>
2025-05-22 12:13:48 +0300

    SSG

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import dynamic from 'next/dynamic'
 import {
   Container,
   Filters,
@@ -9,67 +8,8 @@ import {
 import { Suspense } from "react";
 import { PizzaLoader } from "../components/shared/pizza-loader";
 import { prisma } from "@/prisma/prisma-client";
-const products = [
-  {
-    id: 1,
-    imageUrl:
-      "https://media.dodostatic.net/image/r:292x292/019591b642d87304a62d322945990861.avif",
-    name: "Креветка и песто",
-    items: [{ price: 320 }],
-    count: 1,
-    ingredients:
-      "Креветки, томаты, шампиньоны, соус песто, моцарелла, итальянские травы, фирменный томатный соус",
-  },
-  {
-    id: 2,
-    imageUrl:
-      "https://media.dodostatic.net/image/r:292x292/11ee7d612fc7b7fca5be822752bee1e5.avif",
-    name: "Пеперони фреш",
-    items: [{ price: 280 }],
-    count: 1,
-    ingredients:
-      "Пикантная пепперони, увеличенная порция моцареллы, томаты, фирменный томатный соус",
-  },
-  {
-    id: 3,
-    imageUrl:
-      "https://media.dodostatic.net/image/r:292x292/11ee7d610d2925109ab2e1c92cc5383c.avif",
-    name: "Титан пицца",
-    items: [{ price: 370 }],
-    count: 1,
-    ingredients: "Моцарелла, сыры чеддер и пармезан, фирменный соус альфредо",
-  },
-  {
-    id: 4,
-    imageUrl:
-      "https://media.dodostatic.net/image/r:292x292/11ef5b10b39bbbbda9f8c4e4ff1b067c.avif",
-    name: "Ветчина и грибы",
-    items: [{ price: 300 }],
-    count: 1,
-    ingredients:
-      "Ветчина, шампиньоны, увеличенная порция моцареллы, фирменный томатный соус",
-  },
-  {
-    id: 5,
-    imageUrl:
-      "https://media.dodostatic.net/image/r:292x292/11ee7d6108e3a1c9952cd3a7f39a4d02.avif",
-    name: "Мясная",
-    items: [{ price: 250 }],
-    count: 1,
-    ingredients:
-      "Цыпленок, ветчина, пикантная пепперони, острые колбаски чоризо, моцарелла, фирменный томатный соус",
-  },
-  {
-    id: 6,
-    imageUrl:
-      "https://media.dodostatic.net/image/r:292x292/11ee7d6149eb101d8727573088fa2eff.avif",
-    name: "Диабола",
-    items: [{ price: 320 }],
-    count: 1,
-    ingredients:
-      "Острые колбаски чоризо, острый перец халапеньо, соус барбекю, митболы из говядины, томаты, сладкий перец, красный лук, моцарелла, фирменный томатный соус",
-  },
-];
+
+export const revalidate = 0;
 
 const Home = async () => {
   const categories = await prisma.category.findMany({
