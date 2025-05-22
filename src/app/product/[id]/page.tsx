a76bea7b809493c52c679fae69cc3ade52f56cbb
Anton <96301510+AntonOnoprienko@users.noreply.github.com>
2025-05-22 17:45:32 +0300

    Added ProductId page

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,5 +1,14 @@
-import React from 'react';
-import { cn } from '@/src/lib/utils';
+
+import React from "react";
+import { cn } from "@/src/lib/utils";
+import { prisma } from "@/prisma/prisma-client";
+import { notFound } from "next/navigation";
+import {
+  Container,
+  GroupVariants,
+  ProductImage,
+  Title,
+} from "@/src/components/shared";
 
 type Props = {
   params: {
@@ -7,11 +16,56 @@ type Props = {
   };
 };
 
-const ProductId = ({ params }: Props) => {
+const ProductId = async ({ params }: Props) => {
+  const product = await prisma.product.findFirst({
+    where: { id: Number(params.id) },
+  });
+
+  if (!product) {
+    return notFound();
+  }
+
   return (
-    <div className={cn('')}>
-      <p>Продукт {params.id}</p>
-    </div>
+    <Container className="flex flex-col my-10">
+      <div className="flex flex-1">
+        <ProductImage
+          imageUrl={product.imageUrl}
+          size={40}
+          alt={product.name}
+          className={""}
+        />
+        <div className="w-[490px] bg-[#F7F6F5] p-7">
+          <Title
+            text={product.name}
+            size="md"
+            className="font-extrabold mb-1"
+          />
+          <p className="text-gray-400">
+            Lorem ipsum dolot. Ea distinctio, laborum perferendis libero
+            voluptas, enim sequi fuga
+          </p>
+          <GroupVariants
+            value="2"
+            items={[
+              {
+                name: "Маленькая",
+                value: "1",
+              },
+              {
+                name: "Средняя",
+                value: "2",
+              },
+              { 
+                name: "Большая",
+                value: "3",
+                disabled: true
+              },
+            ]}
+            
+          />
+        </div>
+      </div>
+    </Container>
   );
 };
 
