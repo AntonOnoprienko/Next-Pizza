a36829a35f21b36d19cf7c808bcb8527e27739ee
Anton <96301510+AntonOnoprienko@users.noreply.github.com>
2025-06-29 11:55:47 +0300

    SEO update

--- src/app/(root)/head.tsx
+++ src/app/(root)/head.tsx
@@ -1,21 +1,22 @@
 export default function Head() {
   return (
     <>
-    <link rel="canonical" href="https://next-pizza.up.railway.app/" />
+      <link rel="canonical" href="https://next-pizza.up.railway.app/" />
       <script
         type="application/ld+json"
         dangerouslySetInnerHTML={{
           __html: JSON.stringify({
             "@context": "https://schema.org",
-            "@type": "Organization",
+            "@type": "LocalBusiness",
             "name": "Next Pizza",
+            "image": "https://next-pizza.up.railway.app/og-image.png",
             "url": "https://next-pizza.up.railway.app",
-            "logo": {
-              "@type": "ImageObject",
-              "url": "https://next-pizza.up.railway.app/og-image.png",
-              "width": 112,
-              "height": 112,
-            },
+            "logo": "https://next-pizza.up.railway.app/og-image.png",
+            "address": {
+              "@type": "PostalAddress",
+              "addressLocality": "Харьков",
+              "addressCountry": "UA"
+            }
           }),
         }}
       />
