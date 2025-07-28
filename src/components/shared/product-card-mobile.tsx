ae45d9b389fb6d6520c4b70e9542fba26307ed8d
Anton <96301510+AntonOnoprienko@users.noreply.github.com>
2025-07-28 13:58:09 +0300

    test optimization img size

--- src/components/shared/product-card-mobile.tsx
+++ src/components/shared/product-card-mobile.tsx
@@ -41,7 +41,10 @@ const ProductCardMobileComponent: React.FC<Props> = ({
     <div className={cn('h-full flex gap-2 py-4', className)}>
       <Link href={`/product/${id}`} className="shrink-0">
         <DynamicCldImage
-          src={imageUrl}
+          src={imageUrl.replace(
+            '/upload/',
+            '/upload/w_130,h_130,c_fill,q_auto,f_auto/',
+          )}
           alt={name}
           width={130}
           height={130}
